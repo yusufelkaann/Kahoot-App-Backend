0130f4a1b3df96fcec50824ca9525ce2932251d1
yusufelkaann <elkaanyusuff@gmail.com>
2026-02-19 16:49:15 +0300

    initial commit with quiz related entities

---
+++ src/main/java/com/kahoot_app/Kahoot_App/KahootAppApplication.java
@@ -0,0 +1,13 @@
+package com.kahoot_app.Kahoot_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KahootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kahoot_app/Kahoot_App/KahootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kahoot_app.Kahoot_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
