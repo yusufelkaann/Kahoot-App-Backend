afd06c2bac69a08f142e7e0c9b27855857420ada
yusufelkaann <elkaanyusuff@gmail.com>
2026-04-23 23:23:48 +0300

    delete endpoint for quiz

--- src/main/java/com/kahoot_app/Kahoot_App/controller/QuizController.java
+++ src/main/java/com/kahoot_app/Kahoot_App/controller/QuizController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -55,5 +56,12 @@ public class QuizController {
     @ResponseStatus(HttpStatus.CREATED)
     public QuizResponseDTO generateQuiz(@Valid @RequestBody GenerateQuizRequestDTO request) {
         return aiQuizService.generateQuiz(request);
+    }
+
+    // Delete endpoint
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteQuiz(@PathVariable Long id) {
+        quizService.deleteQuiz(id);
     }
 }
